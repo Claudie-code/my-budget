957abd334ab7c0725ecee5ca54e62a39ce984aba
Claudie Gueguen <gueguen_c@yahoo.fr>
2026-02-07 11:46:53 +0100

    refactor: reorganize QueryProvider in providers folder

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,14 +2,12 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-
-const queryClient = new QueryClient();
+import { QueryProvider } from './providers/QueryProvider.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
+    <QueryProvider>
       <App />
-    </QueryClientProvider>
+    </QueryProvider>
   </StrictMode>,
 );
